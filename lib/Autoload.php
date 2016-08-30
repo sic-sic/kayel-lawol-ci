012e1cd9d2d6fa469fb03696e649d260498585ae
sic-sic <warsicdia@gmail.com>
2016-08-30 07:18:25 +0000

    Autoload.php created to replace the load method in LawolEnvironment

---
+++ lib/Autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'checker/Checker.php';
+
+require_once 'filter/Filter.php';
+require_once 'filter/FilterFunc.php';
+require_once 'filter/FilterParser.php';
+
+require_once 'route/Route.php';
+require_once 'route/RouteFinder.php';
+require_once 'route/RouteProvider.php';
+require_once 'route/ParamFinder.php';
